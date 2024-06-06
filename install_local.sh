11214c7afb216034dc4518dd87d2874fd529f07b
Ryan Giordano <rgiordan@gmail.com>
2024-06-06 14:03:05 -0700

    move it all up a directory

--- install_local.sh
+++ install_local.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 PKG_NAME="mrpaw"
-R -e 'library(devtools); devtools::document("'$PKG_NAME'"); devtools::install_local("'$PKG_NAME'", force=TRUE, upgrade="never")'
+R -e '
+library(devtools); 
+devtools::document("."); 
+devtools::install_local(".", force=TRUE, upgrade="never")
+'
